0902be2d4f2d7c9987d8579eb36ff2290cb21339
Wang Fei <wangfei@hotmail.sg>
2020-05-31 19:58:25 +0800

    Add contact, Change app icon

--- TrialLoginPage/app/src/main/java/com/example/trialloginpage/SplitBill.java
+++ TrialLoginPage/app/src/main/java/com/example/trialloginpage/SplitBill.java
@@ -2,13 +2,41 @@ package com.example.trialloginpage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
 
 public class SplitBill extends AppCompatActivity {
+    public static final int PICK_CONTACT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_split_bill);
+
+        (findViewById(R.id.button)).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+                startActivityForResult(i, PICK_CONTACT);
+            }
+        });
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PICK_CONTACT && resultCode == RESULT_OK) {
+            Uri contactUri = data.getData();
+            Cursor cursor = getContentResolver().query(contactUri, null, null, null, null);
+            cursor.moveToFirst();
+            int column = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+            Log.d("phone number", cursor.getString(column));
+        }
+    }
+
 }
