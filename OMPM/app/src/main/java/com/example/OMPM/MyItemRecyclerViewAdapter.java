745bfcf56163eaee275420ee746f688ef04d2c66
Wang Fei <wangfei@hotmail.sg>
2020-06-28 12:57:21 +0800

    UI for login

--- OMPM/app/src/main/java/com/example/OMPM/MyItemRecyclerViewAdapter.java
+++ OMPM/app/src/main/java/com/example/OMPM/MyItemRecyclerViewAdapter.java
@@ -7,8 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.example.OMPM.dummy.DummyContent.DummyItem;
-
 import java.util.List;
 
 public class MyItemRecyclerViewAdapter extends RecyclerView.Adapter<MyItemRecyclerViewAdapter.ViewHolder> {
--- OMPM/app/src/main/java/com/example/OMPM/OweMeFragment.java
+++ OMPM/app/src/main/java/com/example/OMPM/OweMeFragment.java
@@ -15,10 +15,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.OMPM.dummy.DummyContent;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
