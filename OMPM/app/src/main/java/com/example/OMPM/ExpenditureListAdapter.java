d2f6c3700f627ce4953a40b9043ff5f4ce406a24
chengseong <chengseong02@hotmail.com>
2020-07-20 16:22:11 +0800

    Updated Expenditure History to show money properly

--- OMPM/app/src/main/java/com/example/OMPM/ExpenditureListAdapter.java
+++ OMPM/app/src/main/java/com/example/OMPM/ExpenditureListAdapter.java
@@ -26,6 +26,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.NumberFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -125,7 +126,8 @@ public class ExpenditureListAdapter extends RecyclerView.Adapter<ExpenditureList
         }
 
         void bindTo(Expenditure item) {
-            mCostText.setText(item.getCost());
+            NumberFormat formatter = NumberFormat.getCurrencyInstance();
+            mCostText.setText(formatter.format(Float.parseFloat(item.getCost())));
             SimpleDateFormat sdf = new SimpleDateFormat("dd/MMMM/YYYY");
             mDateText.setText(sdf.format(item.getTimestamp()));
             mTypeText.setText(item.getType());
