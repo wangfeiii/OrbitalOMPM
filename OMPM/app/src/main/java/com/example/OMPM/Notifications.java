05afa2901316c5a783c0f8893eeed2bd00b709fc
chengseong <chengseong02@hotmail.com>
2020-07-23 12:35:22 +0800

    Created NotificationListAdapter

--- OMPM/app/src/main/java/com/example/OMPM/Notifications.java
+++ OMPM/app/src/main/java/com/example/OMPM/Notifications.java
@@ -1,14 +1,30 @@
 package com.example.OMPM;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class Notifications extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private NotificationListAdapter mAdapter;
+    ArrayList<Debt> mDebtList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications);
+
+        mRecyclerView = findViewById(R.id.recyclerview);
+        mAdapter = new NotificationListAdapter(mRecyclerView.getContext(), mDebtList);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(mRecyclerView.getContext()));
+        DividerItemDecoration itemDecor = new DividerItemDecoration(mRecyclerView.getContext(), DividerItemDecoration.VERTICAL);
+        mRecyclerView.addItemDecoration(itemDecor);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
