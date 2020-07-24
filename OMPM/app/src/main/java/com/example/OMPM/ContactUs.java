867621931585f386d42cd694681ca5b990fd61e2
Fei <admin@admins-MacBook-Pro.local>
2020-07-24 18:33:08 +0800

    contact us

--- OMPM/app/src/main/java/com/example/OMPM/ContactUs.java
+++ OMPM/app/src/main/java/com/example/OMPM/ContactUs.java
@@ -3,6 +3,8 @@ package com.example.OMPM;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 public class ContactUs extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class ContactUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
+        TextView text = findViewById(R.id.address);
+      /* text.setText(Html.fromHtml("Address" + "<font size='200'>" +
+                      "123 ABC Road #01-01 Singapore 123456" +
+                        "<font size= '10'>" +
+                "Temperature: "+ "<small> <font color='#59c3fa'>" + "\n" + "temperature" + "°C</font></small>"));
+
+       */
     }
 }
