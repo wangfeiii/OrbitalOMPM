bdd157b1806ad8a8c4d5a99fcbef01d148812f51
chengseong <chengseong02@hotmail.com>
2020-07-24 10:02:00 +0800

    Notifications Page WIP

--- OMPM/app/src/main/java/com/example/OMPM/NotificationsPage.java
+++ OMPM/app/src/main/java/com/example/OMPM/NotificationsPage.java
@@ -27,30 +27,32 @@ public class NotificationsPage extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private NotificationListAdapter mAdapter;
     ArrayList<Debt> mDebtList = new ArrayList<>();
+    private String phoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications);
 
+
         DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        phoneNumber = user.getPhoneNumber();
+
         mDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for (DataSnapshot ds: dataSnapshot.child("users").child(user.getUid()).child("owedTo").getChildren()) {
                     String key = ds.getKey();
                     DataSnapshot sc = dataSnapshot.child("debts").child(key);
-                    String creditor = String.valueOf(sc.child("name").getValue());
-                    for (DataSnapshot debtor : sc.child("debtors").getChildren()) {
-                        Map<String, String> map = (HashMap<String, String>)debtor.getValue();
-                        String paid = String.valueOf(sc.child("creditor").child("paid").getValue());
-                        Debt debt = new Debt(key, String.valueOf(sc.child("amount").getValue()), String.valueOf(sc.child("date").getValue()), map.get("phone"),creditor,Boolean.parseBoolean(paid));
-                        if (paid.equals(false)) {
-                            mDebtList.add(debt);
-                            Log.d(TAG, mDebtList.get(0).getAmount());
-                        }
-                    }
+                    Log.d(TAG, String.valueOf(sc.child("debtors").child(phoneNumber).child("paid").getValue()));
+//                    if (sc.child("debtors").child(phoneNumber).child("paid").getValue().equals(false)) {
+//                        String num = String.valueOf(sc.child("creditor").child("phone").getValue());
+//                        String name = String.valueOf(sc.child("creditor").child("name").getValue());
+//                        String paid = String.valueOf(sc.child("debtors").child(user.getPhoneNumber()).child("paid").getValue());
+//                        Debt debt = new Debt(key,String.valueOf(sc.child("amount").getValue()), String.valueOf(sc.child("date").getValue()), num,name,Boolean.parseBoolean(paid));
+//                        mDebtList.add(debt);
+//                    }
                 }
                 mRecyclerView = findViewById(R.id.recyclerview);
                 mAdapter = new NotificationListAdapter(mRecyclerView.getContext(), mDebtList);
