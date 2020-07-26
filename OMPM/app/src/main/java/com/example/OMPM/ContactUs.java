34a4077b7b21d6796e892b47c1430d7ccf921d43
Fei <admin@admins-MacBook-Pro.local>
2020-07-26 21:57:30 +0800

    submit to firebase

--- OMPM/app/src/main/java/com/example/OMPM/ContactUs.java
+++ OMPM/app/src/main/java/com/example/OMPM/ContactUs.java
@@ -4,20 +4,71 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ContactUs extends AppCompatActivity {
+    private FirebaseUser user;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
         setContentView(R.layout.activity_contact_us);
         TextView text = findViewById(R.id.address);
-      /* text.setText(Html.fromHtml("Address" + "<font size='200'>" +
-                      "123 ABC Road #01-01 Singapore 123456" +
-                        "<font size= '10'>" +
-                "Temperature: "+ "<small> <font color='#59c3fa'>" + "\n" + "temperature" + "°C</font></small>"));
 
-       */
+        final EditText me = findViewById(R.id.name);
+        me.setText(user.getDisplayName());
+
+        final EditText subjectET = findViewById(R.id.subject);
+
+        final EditText emailET = findViewById(R.id.email);
+
+        final EditText messageET = findViewById(R.id.message);
+
+
+        findViewById(R.id.submit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String myName = me.getText().toString();
+                String subject = subjectET.getText().toString();
+                String email = emailET.getText().toString();
+                String message = messageET.getText().toString();
+
+                if (subject.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Please key in subject!",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+
+                } else if (email.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Please key in email!",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+
+                }  else if (message.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Please key in message!",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                String key = mDatabase.child("contactUs").push().getKey();
+                mDatabase.child("contactUs").child(key).child("name").setValue(myName);
+                mDatabase.child("contactUs").child(key).child("subject").setValue(subject);
+                mDatabase.child("contactUs").child(key).child("email").setValue(email);
+                mDatabase.child("contactUs").child(key).child("message").setValue(message);
+            }
+        });
+
     }
 }
